36b7302de4bf8806a1b1ec05f9b7f20ced6b677d
Mightymanh <dphan8@uic.edu>
2025-03-29 18:34:21 -0500

    push button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import BasicButtonExample from './components/BasicButtonExample';
 import Navbar from './Navbar/Navbar';
 import { BrowserRouter, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ function App() {
   return (
       <div className="app-background">
         <Navbar/>
+        <BasicButtonExample />
         <Routes>
             {/* <Route path="/" element={<Navigate to="/home" />} />
             <Route path="home" element={ <Home />} />*/}
