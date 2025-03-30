a0eb69072a5f4d3ae50367a24d1f206d842c780b
Mightymanh <dphan8@uic.edu>
2025-03-30 00:57:13 -0500

    make auth proper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,32 @@ import VaccinationReminders from  './VaccinationReminders/VaccinationReminders'
 import SetReminder from  './SetReminder/SetReminder'
 import WelcomePage from './WelcomePage/Welcome';
 import Home from  './Home/Home'
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  console.log(isAuthenticated)
+
   return (
     <div className="app-background">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="vaccination-reminders" element={ <VaccinationReminders />} />
-        <Route path="set-reminder/:type" element={ <SetReminder />} />
-        <Route path="/home" element={ <Home />} />
-        </Routes>
+      {!isLoading && (
+        <Routes>
+          <Route path="/" element={
+            user? <Navigate to="/home" /> : <WelcomePage />
+          } />
+          <Route path="/vaccination-reminders" element={
+            user? <VaccinationReminders /> : <Navigate to="/" />
+          } />
+          <Route path="/set-reminder/:type" element={ 
+            user? <SetReminder /> : <Navigate to="/" />
+          } />
+          <Route path="/home" element={
+            <Home />
+          } />
+      </Routes>
+    )}
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain="dev-f10vt2crej66q3h4.us.auth0.com"
     clientId="0smfFHZTTLzUhN1G86ZCFkUZMn257yzI"
+    cacheLocation="localstorage" // This ensures the session is saved in localStorage, even after page refresh
     authorizationParams={{
       redirect_uri: `${window.location.origin}/home`
     }}
